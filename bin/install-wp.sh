1be0fa2928370632a2b5ac4c32e29f5424f296d8
harshlunia71 <luniaharsh2003@gmail.com>
2025-09-11 00:00:44 +0800

    Update env variable names and proper quoting

--- bin/install-wp.sh
+++ bin/install-wp.sh
@@ -1,40 +1,40 @@
 #!/usr/bin/env sh
 
-function strip_quotes () {
-	echo $* | xargs
+strip_quotes () {
+	echo "$*" | xargs
 }
 
 # Wait for database connection
 echo "waiting for mysql:${MYSQL_EXTERNAL_PORT}"
-until nc -z mysql ${MYSQL_EXTERNAL_PORT}; do
+until nc -z mysql "${MYSQL_EXTERNAL_PORT}"; do
 	sleep 1
 done
 echo "Can find mysql."
 
 # Install Wordpress
 wp core install \
-	--path="/var/www/html" \
-	--url=${WP_EXTERNAL_URL} \
+	--path="${WP_PATH}" \
+	--url="${WP_EXTERNAL_URL}" \
 	--title="$(strip_quotes ${WP_TITLE})" \
 	--admin_user="${WP_ADMIN_USER}" \
 	--admin_password="${WP_ADMIN_PASSWORD}" \
 	--admin_email="${WP_ADMIN_EMAIL}"
 
 # Theme installation
-wp theme install $(strip_quotes ${WP_THEME}) --activate
+wp theme install $(strip_quotes "${WP_THEME}") --activate
 wp theme delete --all
 
 # Delete unwanted plugins
 wp plugin delete akismet hello
 
 # Plugin installation
-wp plugin install $(strip_quotes ${WP_PLUGINS_TO_INSTALL}) --activate
+wp plugin install $(strip_quotes "${WP_PLUGINS}") --activate
 
 # Custom plugin activation
-wp plugin activate ${WP_CUSTOM_PLUGINS_TO_ACTIVATE}
+wp plugin activate "${WP_CUSTOM_PLUGIN}"
 wp plugin auto-updates enable --all
 
-echo -e "\nREPORT\n"
+printf "\nREPORT\n"
 
 # List users
 echo "===User List==="
